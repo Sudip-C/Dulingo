0c6610bb6b3f3b92a9466de47a887b92304e9517
Sudip Chowdhury <rexalex826@gmail.com>
2024-06-29 19:16:13 +0530

    Header done with sign in and sign up

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { ClerkLoading } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col 
+   lg:flex-row items-center justify-center p-4 gap-2">
+    <div className="relative w-[240px] h-[240px] lg:w-[424px]
+      lg:h-[424px] mb-8 lg:mb-0">
+      <Image src="/hero.png" alt="hero" height={2000} width={2000} />
+    </div>
+    <div className="flex flex-col items-center gap-y-8">
+      <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+        Learn, practice, and master new languages with Dulingo.
+      </h1>
+      <div>
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+      </div>
+    </div>
+   </div>
+  )
+}
---
+++ app/buttons/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+const ButtonPage=()=>{
+    return(
+        <div>
+        <Button>Primary</Button>
+        </div>
+    )
+}
+export default ButtonPage
