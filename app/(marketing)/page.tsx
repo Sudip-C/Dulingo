4f6345498bf426d85d551151748e67e21e494787
Sudip Chowdhury <rexalex826@gmail.com>
2024-07-23 13:20:16 +0530

    footer completed

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,8 @@
 import { Button } from "@/components/ui/button";
-import { ClerkLoading } from "@clerk/nextjs";
+import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs";
 import { Loader } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -15,10 +16,39 @@ export default function Home() {
       <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
         Learn, practice, and master new languages with Dulingo.
       </h1>
-      <div>
+      <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
         <ClerkLoading>
           <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
         </ClerkLoading>
+        <ClerkLoaded>
+          <SignedOut>
+            <SignUpButton
+            mode="modal"
+            afterSignInUrl="/learn"
+            afterSignUpUrl="/learn"
+            >
+              <Button size="lg" variant='secondary' className="w-full" >
+                Get Strated
+              </Button>
+            </SignUpButton>
+            <SignInButton
+            mode="modal"
+            afterSignInUrl="/learn"
+            afterSignUpUrl="/learn"
+            >
+              <Button size="lg" variant='primaryOutline' className="w-full" >
+                I already have an account
+              </Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <Button size="lg" variant="secondary" className="w-full" asChild>
+              <Link href='/learn'>
+                Continue Learning
+              </Link>
+            </Button>
+          </SignedIn>
+        </ClerkLoaded>
       </div>
     </div>
    </div>
